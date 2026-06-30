4eb9cb2f1044bedc7ff3454db2f40482736ce22b
林志东 <154854120+Zhidong-Lin@users.noreply.github.com>
2026-06-30 16:04:59 +0800

    Delete src/main/java/TidUtilityList.java

--- src/main/java/TidUtilityList.java
+++
@@ -1,8 +0,0 @@
- public class TidUtilityList {
-    int[] tids;   // 该 item 出现在哪些 tid 中
-    int[] utils;  // 对应的 utility 值
-     public TidUtilityList(int[] tids, int[] utils) {
-        this.tids = tids;
-        this.utils = utils;
-    }
-}
